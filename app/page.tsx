90049b345c65a7dd649981d73b7a99b6186d79a3
manlikexavy <chibokaxavier@gmail.com>
2024-10-26 18:28:32 +0100

    landing page in progress

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,29 @@
 export default function Home() {
   return (
     <main className="sm:max-w-[738px] md:max-w-[1024px] lg:max-w-[1366px] mx-auto h-[100vh] px-5 ">
-      fghjkl
+      <section className="lg:px-20 sm:px-10 ">
+        <div className="xl:h-[550px] h-auto mt-[10px] flex flex-col xl:flex-row xl:gap-[90px]">
+          {/* Text Section */}
+          <div className="xl:w-[50%] mt-[20px] lg:mt-[80px]">
+            <h2 className="font-bold lg:text-5xl  text-3xl sm:text-4xl md:text-[44px]  leading-[50px] sm:leading-[55px]  capitalize lg:mb-5">
+             Find real estate and get your dream place.
+            </h2>
+            <p className="py-5 text-sm sm:text-base md:text-lg md:leading-[40px] ">
+            Welcome to your journey home! Whether you're searching for a cozy apartment, a family-friendly house, or a luxury getaway, our app connects you with properties that suit your lifestyle and budget. Browse through curated listings, explore neighborhoods.
+            </p>
+           
+          </div>
+
+          {/* Image Section */}
+          <div className="flex justify-center items-center xl:w-[50%]  xl:mb-36">
+            <img
+              src="/house.png"
+              className="  md:w-[500px] md:h-[500px] lg:h-[600px] lg:w-[600px] object-contain"
+              alt="Food Illustration"
+            />
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
